af30b1a7adc557b87991b1a72097b468c5e91b5c
Pietari Eronen <pietari.eronen@nitro.fi>
2022-06-20 10:40:28 +0300

    Init

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,27 +2,27 @@ import React from "react";
 
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
+      <h1 className="text-6xl font-bold leading-tight tracking-tighter md:text-8xl md:pr-8">
         Blog.
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      {/* <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
         A statically generated blog example using{' '}
         <a
           href="https://www.gatsbyjs.com/"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="underline transition-colors duration-200 hover:text-success"
         >
           Gatsby
         </a>{' '}
         and{' '}
         <a
           href="https://www.datocms.com/"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="underline transition-colors duration-200 hover:text-success"
         >
           DatoCMS
         </a>
         .
-      </h4>
+      </h4> */}
     </section>
   )
 }
